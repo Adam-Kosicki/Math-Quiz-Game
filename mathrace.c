8faa5d21e8ac6485525205debb219c22392c49ab
Adam Kosicki <100010326+Adam-Kosicki@users.noreply.github.com>
2023-08-07 21:52:10 -0500

    Update mathrace.c

--- mathrace.c
+++ mathrace.c
@@ -5,34 +5,45 @@
 #include <unistd.h>
 #include <signal.h>
 
-int counter=0;
+int counter = 0; // Variable to store the user's score
 
+// Function to handle the alarm signal
 void sig_alarm(int signo) {
     printf("Your score: %d\n", counter);
     exit(0);
-}	
-
-
-int main() {
-    srand(time(NULL) + getpid() + getuid());
-    alarm(20);
-    while (1) {
-        //generate 2 random numbers in the range of 1-1000
-        int num1 = rand() % 1000 + 1;
-        int num2 = rand() % 1000 + 1;
-        //prompt the user
-        printf( "%d + %d ? ", num1, num2);
-        int result = num1 + num2;
-        int input;
-        if (scanf("%d", &input) == 1)
-            if (result == input){
-                puts("Great!");
-		counter++;
-	    }
-            else
-                printf( "Not quite. Answer is %d.\n", result);
-        else
-            exit(1);
-    }
 }
 
+int main() {
+    // Seed the random number generator using a combination of time, process ID, and user ID
+    srand(time(NULL) + getpid() + getuid());
+
+    // Set up an alarm signal to trigger after 20 seconds
+    alarm(20);
+
+    // Infinite loop to generate and present addition problems
+    while (1) {
+        // Generate two random numbers in the range of 1-1000
+        int num1 = rand() % 1000 + 1;
+        int num2 = rand() % 1000 + 1;
+
+        // Prompt the user with the addition problem
+        printf("%d + %d ? ", num1, num2);
+        int result = num1 + num2; // Calculate the correct result
+        int input;
+
+        // Read the user's input
+        if (scanf("%d", &input) == 1) {
+            // Compare the user's input with the correct result
+            if (result == input) {
+                puts("Great!");
+                counter++; // Increment the score
+            } else {
+                printf("Not quite. Answer is %d.\n", result);
+            }
+        } else {
+            exit(1); // Exit the program if input is not read properly
+        }
+    }
+
+    return 0;
+}
